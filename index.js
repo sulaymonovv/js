8c1cdbc80760d496d8657b39398ea2a8a4e62308
Otabek0000 <otabekdavbaqov2@gmail.com>
2024-02-21 14:14:43 +0500

    js finished

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+var a = prompt("ismingizni kiriting")
+var b = prompt("familiyangizni kiriting")
 
+
+console.log("sizning ismingizda", a.length, "ta harif bor");
+console.log("sizning familiyangizda", b.length,"ta harif bor");
+console.log("hamasi bolib",+a.length + + b.length, "ta");
